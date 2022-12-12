74b1f96c14bc58d25c896be7f1879e0b6344fd10
eJwtjL0KwjAYAN8lc4fUYJtmExcREcTfLaTNFw2NTfzSakV8d4N0Ow7uPkQ1DcR48C10RBCq+QyAGQaM55zPWFUZRaskGOOmqHXJK2oKSjIS0D+tBpzSz3SS/bS63rykR7svcHm6XFw9xvPmsd2qlV2WoV0v1jnlMN+NafVPZP8OkLoaFAImGxv/F0rfbSfCUDvbyBbeGYLSwuM1QfDZEAEF3JV12ctja5x/pRjGYBGiVD0R3eDc9/sD+eVPiA== <laispintodasilva@gmail.com>
2022-12-12 10:19:29 -0300

    Commit inicial.

--- Exercicio 2 - Projeto para acender 1 LED/Exercício 2 - acender 1 LED.ino
+++ Exercicio 2 - Projeto para acender 1 LED/Exercício 2 - acender 1 LED.ino
@@ -1,17 +1,16 @@
 int variableOne = 2;
 int variableTwo = 4;
+
 void setup() {
-  // put your setup code here, to run once:  
   pinMode(variableOne, OUTPUT);
   pinMode(variableTwo, OUTPUT);
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
   digitalWrite(variableOne, HIGH);
-  digitalWrite(variableTwo, HIGH);
-  delay(200);
   digitalWrite(variableTwo, LOW);
+  delay(200);
+  digitalWrite(variableTwo, HIGH);
   digitalWrite(variableOne, LOW);
   delay(200);
 }
