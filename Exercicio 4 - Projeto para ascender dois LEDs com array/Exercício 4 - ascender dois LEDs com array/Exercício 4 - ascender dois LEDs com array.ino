1dfbfe43164444edd2de4f29ce01a3e0ffab4122
LaisPinto <44181586+LaisPinto@users.noreply.github.com>
2022-12-13 14:14:52 -0300

    Update Exercício 4 - ascender dois LEDs com array.ino

--- Exercicio 4 - Projeto para ascender dois LEDs com array/Exercício 4 - ascender dois LEDs com array/Exercício 4 - ascender dois LEDs com array.ino
+++ Exercicio 4 - Projeto para ascender dois LEDs com array/Exercício 4 - ascender dois LEDs com array/Exercício 4 - ascender dois LEDs com array.ino
@@ -1,26 +1,26 @@
-int wait = 100;
-int pin[] = {2, 3};
-int numberPins = 2;
+int pinLed []= {2,4};
 
 void setup() {
-  for (int pinArduino = 0; pinArduino < numberPins; pinArduino++) {
-    pinMode(pin[pinArduino], HIGH);
+  
+  Serial.begin(9600);
+  for(int i = 0; i <= 1; i++){
+    pinMode(pinLed[i], OUTPUT);
   }
+  
 }
 
 void loop() {
-  for (int pinArduino = 0; pinArduino < numberPins; pinArduino++) {
-    digitalWrite(pin[pinArduino], HIGH);
-    delay(wait);
-    digitalWrite(pin[pinArduino], LOW);
-  }
-
-  for (int pinsArduino = numberPins - 1; pinsArduino >= 0; pinsArduino--) {
-    digitalWrite(pin[pinsArduino], HIGH);
-    delay(wait);
-    digitalWrite(pin[pinsArduino], LOW);
+  
+  if(digitalRead(pinLed[0]) == 0) {
+    
+  digitalWrite(pinLed[1], HIGH);
+  digitalWrite(pinLed[0], HIGH);
+  delay(300);
+  
+  } else {
+    
+  digitalWrite(pinLed[1], LOW);
+  digitalWrite(pinLed[0], LOW);
+  delay(300);      
   }
 }
-
-
-
